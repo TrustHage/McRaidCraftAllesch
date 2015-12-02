a50bfa0c01f9e024d80d7f5b19b23a0cd3b8ff81
TrustHage <deathhackz@gmail.com>
2015-12-02 15:04:45 +0100

    *UPDATE* | Now you can kick players with the server console

--- McRaidCraft Allesdoener/src/me/trusthage/allesch/commands/KickCommand.java
+++ McRaidCraft Allesdoener/src/me/trusthage/allesch/commands/KickCommand.java
@@ -15,10 +15,37 @@ public class KickCommand implements CommandExecutor
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
 	{
 		
-		if(!(sender instanceof Player)) return false;
-		Player player = (Player)sender;
-		
-		if(args.length == 0){
+		if(!(sender instanceof Player)) {
+			if(args.length == 0){
+				sender.sendMessage(ChatColor.RED + "Please type /kick <playername> and if you want you can add a ban reason");
+			}
+			
+			if(args.length == 1){
+			@SuppressWarnings("deprecation")
+			Player target = Bukkit.getServer().getPlayer(args[0]);
+				if(target != null){
+					sender.sendMessage(ChatColor.GOLD + "You've kicked: " + ChatColor.RED + target.getName());
+					target.kickPlayer("You're kicked by an admin");
+				}else{
+					sender.sendMessage(ChatColor.RED + "That player is not online");
+				}
+		}else{
+			if(args.length >= 2){
+				Server s = Bukkit.getServer();
+				@SuppressWarnings("deprecation")
+				Player target = s.getPlayer(args[0]);
+					if(target != null){
+						sender.sendMessage(ChatColor.GOLD + "You've kicked: " + ChatColor.RED + target.getName() + ChatColor.GOLD + "reason: " + ChatColor.RED + args[1]);
+						target.kickPlayer(args[1]);
+						s.broadcastMessage(ChatColor.GOLD + "The player: " + ChatColor.RED + target.getName() + ChatColor.GOLD + "has been kicked, reason: " + ChatColor.RED + args[1]);
+					}else{
+						sender.sendMessage(ChatColor.RED + "That player is not online");
+					}
+				}
+			}
+		}else{
+			if(args.length == 0){
+			Player player = (Player)sender;
 			if (player.hasPermission(Main.plugin.getConfig().getString("KickBanPermission"))){
 				player.sendMessage(ChatColor.RED + "Please type /kick <playername> and if you want you can add a ban reason");
 			}else {
@@ -26,6 +53,9 @@ public class KickCommand implements CommandExecutor
 			}
 			
 		}
+	
+		
+		
 		
 		if(args.length == 1){
 			@SuppressWarnings("deprecation")
@@ -58,8 +88,7 @@ public class KickCommand implements CommandExecutor
 				}
 			}
 		}
-			
-		
+	}
 		return false;
 	}
 }
